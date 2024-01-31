557074391bc268f368015711e2ebcc0cb3e599ca
Alexandre Vergnaud <alexandre.vergnaud@insa-lyon.fr>
2024-01-31 14:27:47 +0100

    Pre-local test before the end of the project

--- app/routes.py
+++ app/routes.py
@@ -1,102 +1,211 @@
-from flask import render_template, request, redirect, url_for, flash
+#----- IMPORTS -----#
+
+from flask import render_template, request
 from app import app
 from werkzeug.utils import secure_filename
 import os
 from .settings import *
 from .global_methods import *
-from langchain import HuggingFaceHub
-from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.vectorstores import Qdrant
+from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chains.summarize import load_summarize_chain
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langdetect import detect
 from langchain.chains import RetrievalQA
-from langchain.vectorstores import Qdrant
-from qdrant_client import QdrantClient
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from flask import session
 
-messages = [{'a' : "Bonjour, comment puis-je vous aider ?"}]
+#----- INTERACTION WITH THE INTERFACE -----#
 
+# History's dictionnary of messages in the interface with 2 types of keys :
+# 'a' for answers from the llm
+# 'q' for questions from the user
+if LANGUAGE_CHOICE == "fr" :
+    messages = [{'a' : "Bonjour, comment puis-je vous aider ?"}]
+elif LANGUAGE_CHOICE == "en" :
+    messages = [{'a' : "Hi, how can I help ?"}]
+else :
+    raise Exception
+
+# Uploaded documents folder
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-#os.environ["OPENAI_API_KEY"] = "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"
-
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+app.secret_key = 'session_key'
+
 @app.route('/', methods = ['GET', 'POST'])
 def chat():
+    
+    if 'filename' not in session.keys() :
+        session['filename'] = None
+        session['chunksize'] = None
+        session['chunkoverlap'] = None
 
-    global messages
-
+    # Launch of the user's request
     if request.method == 'POST':
 
-        # Récupère le message de l'utilisateur depuis le formulaire
+        # API solution
+        if BOOL_API :
+            os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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" #HuggingFace key
+            llm = HuggingFaceHub(repo_id = "mistralai/Mistral-7B-v0.1")
+
+        # Local solution : loads the LLM locally if not already loaded in the "locl_llm" folder
+        else :
+            if BOOL_LLM :
+                model = AutoModelForCausalLM.from_pretrained(LLM_FOLDER)
+                tokenizer = AutoTokenizer.from_pretrained(LLM_FOLDER)
+            else :
+                model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+                tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+                model.save_pretrained(LLM_FOLDER)
+                tokenizer.save_pretrained(LLM_FOLDER)
+
+            # Creating a runnable LLM
+            pipe = pipeline("text-generation", model = model, tokenizer = tokenizer, max_new_tokens = 500)
+            llm = HuggingFacePipeline(pipeline = pipe)
+
+        # Getting the user's message from the interface
         question_content = request.form.get('message')
-        file = request.files.get('fichier')
-        if question_content and not file:
 
-            llm = HuggingFaceHub(repo_id = "mistralai/Mistral-7B-Instruct-v0.1")
+        # Getting the file uploaded form the interface, saving it and choosing the right parameters
+        file_article = request.files.get('file_article')
+        file_lesson = request.files.get('file_lesson')
+        if file_article or file_lesson :
 
-            messages.append({'q' : question_content, 'a' : llm(question_content)})  # Ajoute le message à la liste
+            if file_article and allowed_file(file_article.filename) :
+                filename = secure_filename(file_article.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER']+"/unprocessed", filename)
+                file_article.save(file_path)
+                chunk_size = 500
+                chunk_overlap = 10
 
-        # Gestion du téléchargement des fichiers
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # Assurez-vous que le nom de fichier est sûr
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)  # Sauvegarde le fichier dans le dossier de destination
+            elif file_lesson and allowed_file(file_lesson.filename) :
+                filename = secure_filename(file_lesson.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER']+"/unprocessed", filename)
+                file_article.save(file_path)
+                chunk_size = 500
+                chunk_overlap = 10
 
-            # Identifier et ajouter le type de fichier au message
+            # Tell's the user that the file has been correctly uploaded
             file_type_description = determine_file_type(filename)
-            messages.append({'a' : f"Le fichier '{filename}' a été correctement téléchargé, c'est {file_type_description}."})
-
-            # Text load
-            loader = TextLoader(file_path)
+            if LANGUAGE_CHOICE == "fr" :
+                messages.append({'a' : f"Le fichier '{filename}' a été correctement téléchargé, c'est {file_type_description}."})
+            elif LANGUAGE_CHOICE == "en" :
+                messages.append({'a' : f"The file '{filename}' has been correctly uploaded, it's {file_type_description}."})
+            else :
+                raise Exception
+            
+            # Loads the text with the according loader (text or PDF)
+            if filename.rsplit('.', 1)[1].lower() == "txt" :
+                loader = TextLoader(file_path)
+            elif filename.rsplit('.', 1)[1].lower() == "pdf" :
+                loader = PyPDFLoader(file_path)
             pages = loader.load()
 
-            if not question_content :
-                # LLM : french and english
-                try :
-                    language = detect(pages[0].page_content)
-                    if language == 'fr' :
-                        print('fr')
-                        llm = HuggingFaceHub(repo_id = "plguillou/t5-base-fr-sum-cnndm")
-                    elif language == 'en' :
-                        print('en')
-                        llm = HuggingFaceHub(repo_id = "Falconsai/medical_summarization")
-                    else :
-                        raise Exception
-                except :
-                    raise Exception
+            # Cleaning of the text
+            process_text_file(pages[0].page_content, app.config['UPLOAD_FOLDER']+"/processed/"+filename)
 
-                # Prompt
-                prompt_template = """Write a concise summary of the following text delimited by triple backquotes.
-                Return your response in bullet points which covers the key points of the text.
-                ```{text}```
-                BULLET POINT SUMMARY:
+            # Saving the text and parameters
+            session['filename'] = filename
+            session['chunksize'] = chunk_size
+            session['chunkoverlap'] = chunk_overlap
+            
+        else :
+            
+            # Retrieves the previous text and parameters
+            filename = session['filename']
+            chunk_size = session['chunksize']
+            chunk_overlap = session['chunkoverlap']
+
+        # Getting the summary's buttons clicks
+        summary_type = request.form.get('summary_button')
+
+        # Getting the question's button click
+        question_click = request.form.get('send_button')
+
+        # Case of a simple question to the LLM without any file
+        if question_content and not filename:
+
+            # Adds the question and the computed answer to the chat
+            messages.append({'q' : question_content, 'a' : llm(question_content)})
+
+        # Case of an uploaded file
+        if filename :
+
+            # Loads the text
+            with open(app.config['UPLOAD_FOLDER']+"/processed/"+filename) as source_file :
+                source_text = source_file.read()
+
+            # Preprocessing of the text : splitting, embeddings & vectorstores
+            # Splitting
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
+            texts = text_splitter.create_documents([source_text])
+            splitted_texts = text_splitter.split_documents(texts)
+
+            # Embeddings
+            embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-mpnet-base-v2")
+
+            # Vectorstores
+            vectorstore = Qdrant.from_documents(splitted_texts, embeddings, location = ":memory:")
+
+            # Creates a prompt with the query and the context
+            prompt_template = """Use the following pieces of context to answer the question at the end. 
+            If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+            {context}
+            Question: {question}
+            Helpful Answer:"""
+            prompt = PromptTemplate.from_template(prompt_template)
+
+            # Summarize part
+            if summary_type :
+
+                # Retrieves the type of summary
+                if summary_type == "Short" :
+                    max_words = "100"
+                elif summary_type == "Medium" :
+                    max_words = "250"
+                else :
+                    max_words = "500"
+
+                # Creates the right summary query
+                query = """Write a """+summary_type.lower()+""" summary of the text with less than """+max_words+""" words. \n
+                Return your response in """+LANGUAGE_DICT[LANGUAGE_CHOICE]+""" in bullet points which covers the key points of the text.
                 """
-                prompt = PromptTemplate.from_template(prompt_template)
-
-                # Stuff
-                stuff_chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
+            
+                # Retriever in the vectorstore
+                retriever = vectorstore.as_retriever()
+                runner = RetrievalQA.from_chain_type(llm = llm, retriever = retriever, 
+                                            verbose = True, return_source_documents = True,
+                                            chain_type_kwargs = {"prompt": prompt})
                 
-                messages.append({'a' : stuff_chain.run(pages)})
-
+                # Run the query and adds it into the answers of the interface
+                messages.append({'q' : query, 'a' : runner({"query": query})['result']})
+            
+            # Question-Answering part
+            elif question_click :
+            
+                # Retriever in the vectorstore
+                retriever = vectorstore.as_retriever()
+                runner = RetrievalQA.from_chain_type(llm = llm, retriever = retriever, 
+                                            verbose = True, return_source_documents = True,
+                                            chain_type_kwargs = {"prompt": prompt})
+                
+                # Run the query and adds it into the answers of the interface
+                messages.append({'q' : question_content, 'a' : runner({"query": question_content})['result']})
+            
             else :
-                messages.append({'q' : question_content})
+                raise Exception
+        
+        else :
+            
+            # Tells to upload a file if no file was uploaded
+            if LANGUAGE_CHOICE == "fr" :
+                messages.append({'a' : "Veuillez télécharger un fichier en premier lieu."})
+            else :
+                messages.append({'a' : "Please upload a file first."})
 
-                text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-                docs = text_splitter.split_documents(pages)
-                model_name = "sentence-transformers/all-mpnet-base-v2"
-                embeddings = HuggingFaceEmbeddings(model_name=model_name)
-                llm = HuggingFaceHub(repo_id = "mistralai/Mistral-7B-Instruct-v0.1")
-
-                vectordb = Qdrant.from_documents(documents=docs, embedding=embeddings, location=":memory:", 
-                                                prefer_grpc=True, collection_name="my_documents")
-                retriever = vectordb.as_retriever()
-                qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, verbose=True)
-
-                messages.append({'a' : qa.run(question_content)})
 
     return render_template('chat.html', messages = messages)
--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,22 @@
 import os
 
+# Base directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Language : only french ("fr") and english ("en") available
+LANGUAGE_CHOICE = "fr"
+LANGUAGE_DICT = {"fr" : "french", "en" : "english"}
+
+# Upload directory
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
-ALLOWED_EXTENSIONS = {'pdf', 'txt', 'docx'}
+
+# Allowed extensions for the upload
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
+
+# Interface mode : local by default
+BOOL_API = True
+
+# LLM load and location
+BOOL_LLM = True
+LLM_FOLDER = "local_llm/mistralai/Mistral-7B-v0.1"
+EMBEDDINGS_FOLDER = "local_llm/sentence-transformers/all-mpnet-base-v2"
